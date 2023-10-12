a27a5f7d9f90baf9baaa855cdb1b168b1e0ec057
Lezequiel <deortiz40@gmail.com>
2023-10-11 21:00:48 -0300

    Se suben cambios

--- src/main/java/com/fashion/web/controlador/UsuarioControlador.java
+++ src/main/java/com/fashion/web/controlador/UsuarioControlador.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import com.fashion.web.entidades.Imagen;
 import com.fashion.web.entidades.Usuario;
@@ -24,12 +22,7 @@ import com.fashion.web.servicios.ImagenServicio;
 import com.fashion.web.servicios.UsuarioServicio;
 
 @Controller
-<<<<<<< HEAD
 @CrossOrigin(origins="*")
-=======
-// @RestController
-@CrossOrigin(origins = "*")
->>>>>>> f06467c7e301194b8d659bf05ccb08a0c8d8ea9f
 @RequestMapping("/usuario")
 public class UsuarioControlador {
 
