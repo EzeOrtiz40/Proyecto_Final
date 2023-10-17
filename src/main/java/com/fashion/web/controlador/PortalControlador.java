ecdb2bc5645b7efdbcde4cd1168b06aca304a2a8
Lezequiel <deortiz40@gmail.com>
2023-10-17 18:13:00 -0300

    Se agregan funcionalidades al perfil de usuario

--- src/main/java/com/fashion/web/controlador/PortalControlador.java
+++ src/main/java/com/fashion/web/controlador/PortalControlador.java
@@ -2,19 +2,26 @@ package com.fashion.web.controlador;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fashion.web.entidades.Usuario;
+import com.fashion.web.servicios.UsuarioServicio;
 
 @Controller
 @RequestMapping("/")
 public class PortalControlador {
     
+    @Autowired
+    UsuarioServicio usuarioServicio;
     @GetMapping("/")
     public String Index() {
         return "index";
@@ -50,4 +57,25 @@ public class PortalControlador {
     public String registrar() {
         return "usuario_form";
     }
+
+    @GetMapping("/{id}/perfil")
+    public String perfilUsuario(@PathVariable String email, ModelMap modelo) {
+        
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        Usuario usuario = usuarioServicio.buscarPorEmail(email);
+
+        if (!usuario.getEmail().equals(username)) {
+            
+            return "redirect: index";
+        }
+
+        modelo.addAttribute("nombre", usuario.getNombre());
+        modelo.addAttribute("apellido", usuario.getApellido());
+        modelo.addAttribute("email", usuario.getEmail());
+        
+        return "perfil_usuario"; // El nombre de la vista de perfil de usuario
+    }
+
 }
