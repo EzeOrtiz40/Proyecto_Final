a998ff142c44735a59293ac75edbf06244c9e623
miguel <miguel.jarecca@gmail.com>
2023-10-21 11:48:54 -0500

    Se aumento las categorias en el index

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,4 +1,4 @@
-//Para la dinamica de comentar un publicacion
+//PARA LA DINAMICA DE COMENTAR UNA PUBLICACION
 function mostrarDiv() {
     document.getElementById("miDiv").style.display = "block";
 }
@@ -7,7 +7,7 @@ function ocultarDiv() {
     document.getElementById("miDiv").style.display = "none";
 }
 
-// para el boton de like 
+// PARA EL BOTON DR LIKE 
 function toggleLike() {
     var likeButton = document.querySelector('.button-like');
     var likeIcon = likeButton.querySelector('i');
@@ -23,3 +23,23 @@ function toggleLike() {
         likeIcon.classList.add('fa-regular');
     }
 }
+
+//DINAMICA PARA EL MENU
+// const toggle = document.querySelector(".toggle");
+// const menuDasboard = document.querySelector(".menu-dashboard");
+const iconoMenu = document.querySelector("i");
+const enlacesMenu = document.querySelector(".enlaces");
+
+const toggle = document.getElementById('toggle');
+const menuDashboard = document.getElementById('menuDasboard');
+
+toggle.addEventListener("click",()=> {
+    menuDashboard.classList.toggle("open")
+
+        if (iconoMenu.classList.contains('fa-bars')) {
+            iconoMenu.classList.replace('fa-bars','fa-square-xmark');
+
+        } else {
+            iconoMenu.classList.replace('fa-square-xmark', 'fa-bars');
+        }
+})
